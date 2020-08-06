748593ddb9911ff20282948372fcb15d70940bcf
Goncalo Correia <goncalopcorreia@hotmail.com>
2020-08-06 13:16:29 +0100

    Created validation for name and fiscal_id and respective unit test

--- app/ClientGateway.py
+++ app/ClientGateway.py
@@ -22,7 +22,11 @@ def retrieve(invoice_id):
 
 
 def valid_name(name):
-    return True
+    regex = '^[_A-zÀ-ú0-9]*((-|\s)*[_A-zÀ-ú0-9])*$'
+    if re.search(regex, name):
+        return True
+    else:
+        return False
 
 
 def valid_email(email):
@@ -34,4 +38,15 @@ def valid_email(email):
 
 
 def valid_fiscal_id(fiscal_id):
-    return True
+    regex_europe = '^((AT)?U[0-9]{8}|(BE)?0[0-9]{9}|(BG)?[0-9]{9,10}|(CY)?[0-9]{8}L|\
+        (CZ)?[0-9]{8,10}|(DE)?[0-9]{9}|(DK)?[0-9]{8}|(EE)?[0-9]{9}|\
+        (EL|GR)?[0-9]{9}|(ES)?[0-9A-Z][0-9]{7}[0-9A-Z]|(FI)?[0-9]{8}|\
+        (FR)?[0-9A-Z]{2}[0-9]{9}|(GB)?([0-9]{9}([0-9]{3})?|[A-Z]{2}[0-9]{3})|\
+        (HU)?[0-9]{8}|(IE)?[0-9]S[0-9]{5}L|(IT)?[0-9]{11}|\
+        (LT)?([0-9]{9}|[0-9]{12})|(LU)?[0-9]{8}|(LV)?[0-9]{11}|(MT)?[0-9]{8}|\
+        (NL)?[0-9]{9}B[0-9]{2}|(PL)?[0-9]{10}|(PT)?[0-9]{9}|(RO)?[0-9]{2,10}|\
+        (SE)?[0-9]{12}|(SI)?[0-9]{8}|(SK)?[0-9]{10})$'
+    if re.search(regex_europe, fiscal_id):
+        return True
+    else:
+        return False
--- tests/test_ClientGateway.py
+++ tests/test_ClientGateway.py
@@ -12,3 +12,14 @@ class Test(TestCase):
         self.assertTrue(ClientGateway.valid_email("teste@teste.pt"))
         self.assertFalse(ClientGateway.valid_email("teste@.pt"))
         self.assertFalse(ClientGateway.valid_email("teste.pt"))
+
+    def test_valid_fiscal_id(self):
+        self.assertTrue(ClientGateway.valid_fiscal_id("999999990"))  # PORTUGAL
+        self.assertTrue(ClientGateway.valid_fiscal_id("99999999"))  # Malta
+        self.assertFalse(ClientGateway.valid_fiscal_id("9"))
+
+    def test_valid_name(self):
+        self.assertTrue(ClientGateway.valid_name("Gonçalo"))
+        self.assertTrue(ClientGateway.valid_name("Jose"))
+        self.assertFalse(ClientGateway.valid_name("asd$"))
+        self.assertFalse(ClientGateway.valid_name("ola!"))
