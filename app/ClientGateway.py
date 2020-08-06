95f7b2ecbd17d17230fe44345f4a5377347b234d
Goncalo Correia <goncalopcorreia@hotmail.com>
2020-08-06 12:12:28 +0100

    Created validation for email and respective unit test

--- app/ClientGateway.py
+++ app/ClientGateway.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from app import app
 from flask import request
@@ -18,3 +19,19 @@ def register(invoice_id):
 def retrieve(invoice_id):
     print(app.config['SQLALCHEMY_DATABASE_URI'])
     return invoice_id, 200
+
+
+def valid_name(name):
+    return True
+
+
+def valid_email(email):
+    regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w+$'
+    if re.search(regex, email):
+        return True
+    else:
+        return False
+
+
+def valid_fiscal_id(fiscal_id):
+    return True
--- tests/test_ClientGateway.py
+++ tests/test_ClientGateway.py
@@ -1,8 +1,14 @@
 import unittest
 from unittest import TestCase
+from app import ClientGateway
 
 
 class Test(TestCase):
-    def test_register(self):
-        # self.fail()
-        self.run()
+    # def test_register(self):
+    #     # self.fail()
+    #     self.run()
+
+    def test_valid_email(self):
+        self.assertTrue(ClientGateway.valid_email("teste@teste.pt"))
+        self.assertFalse(ClientGateway.valid_email("teste@.pt"))
+        self.assertFalse(ClientGateway.valid_email("teste.pt"))
